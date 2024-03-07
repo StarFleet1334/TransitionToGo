8840436edc956a7bfc62878a231b8f0788a846d7
IliaLataria01 <latariailia6@gmail.com>
2024-03-08 01:58:00 +0400

    Pointers

--- main.go
+++ main.go
@@ -6,4 +6,18 @@ var greeting = "Hello world!"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	// declare pointer
+	var ptr *string
+
+	// initialize a greeting
+	greeting := "Hello World!"
+
+	// assign
+	ptr = &greeting
+
+	fmt.Println("Greeting:", greeting)
+	fmt.Println("Address of greeting:", ptr)
+	fmt.Println("Value stored in ptr:", *ptr)
+
 }
