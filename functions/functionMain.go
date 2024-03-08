e8a05d536c73de3d6d6c31f1be9b655af4aab2d0
IliaLataria01 <latariailia6@gmail.com>
2024-03-08 05:05:58 +0400

    Section Functions - Deferred functions

--- functions/functionMain.go
+++ functions/functionMain.go
@@ -55,26 +55,40 @@ func testArea() {
 	if err != nil {
 		// means we got an error
 		fmt.Println("Error:", err)
+		//panic("error")
 		return
 	}
 	fmt.Printf("aread(%v,%v) = %v;\n", x, y, *area)
 
 }
 
+func printMsg(id string, m string) {
+	fmt.Printf("[%v]: %v\n", id, m)
+}
+
 func main() {
 
 	// This is referred to anonymous function
-	sub := func(x int, y int) int {
-		return x - y
-	}
-	fmt.Printf("Sub: %v\n", sub(10, 3))
-	fmt.Printf("Add: %v\n", add(10, 3))
+	//sub := func(x int, y int) int {
+	//	return x - y
+	//}
+	//fmt.Printf("Sub: %v\n", sub(10, 3))
+	//fmt.Printf("Add: %v\n", add(10, 3))
+	//
+	//x := 3
+	//fmt.Printf("Before negation: %v\n", x)
+	//negate(&x)
+	//fmt.Printf("After negation: %v\n", x)
+	//
+	//testArea()
+	//
+	//
 
-	x := 3
-	fmt.Printf("Before negation: %v\n", x)
-	negate(&x)
-	fmt.Printf("After negation: %v\n", x)
+	msg := "Hello, world!"
+	defer printMsg("Defer-0", msg)
+	defer printMsg("Defer-1", msg)
 
-	testArea()
+	msg = "Goodbye, world!"
+	fmt.Println("Main has exited.")
 
 }
