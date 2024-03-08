848e2b3fb41fb438fd78fb0a752c756153a8e855
IliaLataria01 <latariailia6@gmail.com>
2024-03-08 22:40:44 +0400

    Methods and receiver functions + pointer

--- structuresAndInterfaces/structsPractice.go
+++ structuresAndInterfaces/structsPractice.go
@@ -13,6 +13,25 @@ func createCity(name string) city {
 	}
 }
 
+func (c city) getNameOfCity() string {
+	return c.name
+}
+
+func (c city) changCityName() {
+	c.name = "Stockholm"
+}
+
+func (c *city) changedCityNameCorrect() {
+	c.name = "Stockholm"
+}
+
+// This would give an error, because method has same name as
+// structs field. That is because city has both field and
+// method name same
+//func (c city) tempC() float64{
+//
+//}
+
 func main() {
 
 	all := city{
@@ -25,4 +44,17 @@ func main() {
 	fmt.Printf("All fields are presented: %v\n", all)
 	fmt.Printf("Some fields are omitted: %v\n", omitted)
 	fmt.Printf("Using function: %v\n", createCity("Los Angeles"))
+
+	fmt.Printf("Name of the city: %s\n", omitted.getNameOfCity())
+
+	// Here we try to change name
+	all.changCityName()
+	// But we see it did not change, cause here we do not use
+	// pointer
+	fmt.Printf("All fields are presented: %v\n", all)
+
+	// Let's call correct one here
+	all.changedCityNameCorrect()
+	fmt.Printf("All fields are presented: %v\n", all)
+
 }
